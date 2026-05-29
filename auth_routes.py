18647328d1274baf5a30557ac37228775fe65782
Robert-FM <robert.fis.fernandes@gmail.com>
2026-05-29 15:56:15 -0300

    Autenticação, OAuth2 e JWT Tokens

--- auth_routes.py
+++ auth_routes.py
@@ -1,16 +1,35 @@
 from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario,db
 from dependencies import pegar_sessao
-from main import bcrypt_context
+from main import bcrypt_context,SECRET_KEY,ALGORITHM,ACCESS_TOKEN_EXPIRE_MINUTES
 from schemas import UsuarioSchema
 from sqlalchemy.orm import Session
 from schemas import LoginSchema
+from jose import jwt, JWTError
+from datetime import datetime, timedelta,timezone
 
 auth_router = APIRouter(prefix='/auth',tags=['auth'])
 
-def criar_token(id_usuario: int):
-    token = f'kdk5889dssç{id_usuario}'
-    return token
+def criar_token(id_usuario: int,duracao_token: int = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
+    data_expiracao = datetime.now(timezone.utc) + duracao_token 
+
+    dict_info = {'sub': str(id_usuario),'exp': data_expiracao}
+    
+    jwt_codificado = jwt.encode(dict_info,SECRET_KEY,algorithm=ALGORITHM)
+    return jwt_codificado
+
+def verificar_token(token: str,session: Session = Depends(pegar_sessao)):
+
+    usuario = session.query(Usuario).filter(Usuario.id == 1).first()
+    return usuario
+
+def autenticar_usuario(email: str, senha: str, session: Session):
+    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+    if not usuario:
+        return False
+    elif not bcrypt_context.verify(senha,usuario.senha):
+        return False
+    return usuario
 
 @auth_router.get('/')
 async def home():
@@ -27,20 +46,34 @@ async def criar_conta(usuario_schemas: UsuarioSchema,session: Session = Depends(
         raise HTTPException(status_code=400,detail='E-mail já cadastrado!')
     else:
         senha_criptografada = bcrypt_context.hash(usuario_schemas.senha)
-        novo_usuario = Usuario(usuario_schemas.nome,usuario_schemas.email,senha_criptografada,
-                               usuario_schemas.ativo,usuario_schemas.admin)
+        novo_usuario = Usuario(usuario_schemas.nome,
+                               usuario_schemas.email,
+                               senha_criptografada,
+                               usuario_schemas.ativo,
+                               usuario_schemas.admin)
         session.add(novo_usuario)
         session.commit()
         return {'Mensagem':f'Conta criada com sucesso {usuario_schemas.email}!'} 
     
 @auth_router.post('/login')
 async def login(login_schema: LoginSchema,session: Session = Depends(pegar_sessao)):
-    usuario = session.query(Usuario).filter(Usuario.email == login_schema.email).first()
+    usuario = autenticar_usuario(login_schema.email, login_schema.senha, session)
     if not usuario:
         raise HTTPException(status_code=400,detail='E-mail ou senha incorretos!')
     else:
         access_token = criar_token(usuario.id)
+        refresh_token = criar_token(usuario.id,duracao_token=timedelta(days=7))
         return {
+            'access_token': access_token,
+            'refresh_token': refresh_token,
+            'token_type': 'Bearer'
+        }
+    
+@auth_router.post('/refresh')
+async def use_refresh_token(token):
+    usuario = verificar_token(token)
+    access_token = criar_token(usuario.id)
+    return {
             'access_token': access_token,
             'token_type': 'Bearer'
         }

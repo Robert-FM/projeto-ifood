a166133dd6764c9ba69ef02d45afdc48be24f9d6
Robert-FM <robert.fis.fernandes@gmail.com>
2026-06-02 15:18:38 -0300

    Finalização e Schemas para respostas de endpoints

--- order_routes.py
+++ order_routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from dependencies import pegar_sessao,verificar_token
-from schemas import PedidoSchema,ItemPedidoSchema
+from schemas import PedidoSchema,ItemPedidoSchema,ResponsePedidosSchema
 from models import Pedido,Usuario,ItemPedido
+from typing import List
 
 order_router = APIRouter(prefix='/pedidos',tags=['pedidos'],dependencies=[Depends(verificar_token)])
 
@@ -21,7 +22,7 @@ async def criar_pedido(pedido_schema: PedidoSchema,session: Session = Depends(pe
     session.commit()
     return {'Mensagem':f'Pedido criado com sucesso. ID do pedido: {novo_pedido.id}.'}
 
-@order_router.get('/pedido/{id_pedido}')
+@order_router.get('/pedido/cancelar/{id_pedido}')
 async def cancelar_pedido(id_pedido: int, session: Session = Depends(pegar_sessao),usuario: Usuario = Depends(verificar_token)):
     pedido = session.query(Pedido).filter(Pedido.id == id_pedido).first()
     if not pedido:
@@ -67,3 +68,70 @@ async def adicionar_item_pedido(id_pedido: int, item_pedido_schema: ItemPedidoSc
         'pedido': pedido,
         'preco_pedido': pedido.preco
     }
+
+@order_router.post("/pedido/remover-item/{id_item_pedido}")
+async def remover_item_pedido(
+    id_item_pedido: int,
+    session: Session = Depends(pegar_sessao),
+    usuario: Usuario = Depends(verificar_token)
+):
+    item_pedido = session.query(ItemPedido).filter(ItemPedido.id == id_item_pedido).first()
+
+    if not item_pedido:
+        raise HTTPException(status_code=400, detail="Item no pedido não existente")
+
+    pedido = session.query(Pedido).filter(Pedido.id == item_pedido.pedido).first()
+
+    if not usuario.admin and usuario.id != pedido.usuario:
+        raise HTTPException(
+            status_code=401,
+            detail="Você não tem autorização para fazer essa operação"
+        )
+
+    session.delete(item_pedido)
+    pedido.calcular_preco()
+    session.commit()
+
+    return {
+        "mensagem": "Item removido com sucesso",
+        "quantidade_itens_pedido": len(pedido.itens),
+        "pedido": pedido
+    }
+
+#finalizando o pedido
+@order_router.get('/pedido/finalizar/{id_pedido}')
+async def finalizar_pedido(id_pedido: int, session: Session = Depends(pegar_sessao),usuario: Usuario = Depends(verificar_token)):
+    pedido = session.query(Pedido).filter(Pedido.id == id_pedido).first()
+    if not pedido:
+        raise HTTPException(status_code=400, detail="Pedido não encontrado")
+    
+    if not usuario.admin and usuario.id != pedido.usuario:
+        raise HTTPException(status_code=403, detail="Acesso negado. Você não tem permissão para cancelar este pedido.")
+
+    pedido.staus = 'Finalizado'
+    session.commit()
+    return {
+        'Mensagem':f'Pedido com ID {pedido.id} finalizado com sucesso.',
+        'pedido': pedido}
+
+#visualizar o pedido
+@order_router.get('/pedido/{id_pedido}')
+async def visualizar_pedido(id_pedido: int, session: Session = Depends(pegar_sessao),
+                            usuario: Usuario = Depends(verificar_token)):
+    pedido = session.query(Pedido).filter(Pedido.id == id_pedido).first()
+    if not pedido:
+        raise HTTPException(status_code=400, detail="Pedido não encontrado")
+    
+    if not usuario.admin and usuario.id != pedido.usuario:
+        raise HTTPException(status_code=403, detail="Acesso negado. Você não tem permissão para visualizar este pedido.")
+
+    return {
+       'quantidade_itens_pedido': len(pedido.itens),
+       'pedido': pedido
+    }
+
+#visualizar todos os pedidos de 1 usuário
+@order_router.get('/lista_pedidos/pedidos-usuario',response_model=List[ResponsePedidosSchema])
+async def listar_pedidos(session: Session = Depends(pegar_sessao),usuario: Usuario = Depends(verificar_token)):
+        pedidos = session.query(Pedido).filter(Pedido.usuario == usuario.id).all()
+        return pedidos
